fe2a3027ce8a422bac080a41f15ca418fda607f2
unknown <sasha.lartsev@gmail.com>
2025-09-07 15:07:46 +0400

    Add repository

--- Employees/Controllers/EmployeeController.cs
+++ Employees/Controllers/EmployeeController.cs
@@ -1,5 +1,8 @@
 ﻿using Employees.Models;
 using Microsoft.AspNetCore.Mvc;
+using DAL;
+using DAL.Interfaces;
+using QueryParams;
 
 namespace Employees.Controllers
 {
@@ -7,16 +10,25 @@ namespace Employees.Controllers
     [Route("[controller]")]
     public class EmployeeController : ControllerBase
     {
+        private readonly IEmlployeeRepository _eployeeRepository;
+
+        public EmployeeController(IEmlployeeRepository eployeeRepository)
+        {
+            _eployeeRepository = eployeeRepository;
+        }
+
         [HttpPost]
         public async Task<ActionResult<int>> Create([FromBody] EmployeeModel model)
         {
+            var id = await _eployeeRepository.CreateAsync(EmployeeModel.ToEntity(model));
 
-            return Ok(new { id = 1 });
+            return Ok(new { id });
         }
 
         [HttpPost("{id}")]
         public async Task<ActionResult<int>> Delete([FromRoute] int id)
         {
+            await _eployeeRepository.DeleteAsync(id);
 
             return Ok(new { deleted = true });
         }
@@ -24,16 +36,28 @@ namespace Employees.Controllers
         [HttpPut("{id}")]
         public async Task<ActionResult<int>> Update([FromRoute] int id, [FromBody] EmployeeModel model)
         {
+            model.Id = id;
+            await _eployeeRepository.UpdateAsync(EmployeeModel.ToEntity(model));
 
             return Ok(new { deleted = true });
         }
 
         [HttpGet]
-        [Route("filter")]
-        public async Task<ActionResult<int>> Get([FromRoute] int id, [FromBody] EmployeeModel model)
+        [Route("{id}")]
+        public async Task<ActionResult<EmployeeModel>> Get([FromRoute] int id, [FromQuery] EmployeesQueryParams queryParams)
         {
+            var result = await _eployeeRepository.GetAsync(id);
 
-            return Ok(new { deleted = true });
+            return Ok(EmployeeModel.ToModel(result));
+        }
+
+        [HttpGet]
+        [Route("filter")]
+        public async Task<ActionResult<IList<EmployeeModel>>> GetList([FromRoute] int id, [FromQuery] EmployeesQueryParams queryParams)
+        {
+            var result = await _eployeeRepository.GetAsync(queryParams);
+
+            return Ok(result.Select(EmployeeModel.ToModel).ToList());
         }
     }
 }
