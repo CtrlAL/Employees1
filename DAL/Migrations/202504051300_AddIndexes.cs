22a90dd7068fa81fa46dc32956b3f3529fdc66de
unknown <sasha.lartsev@gmail.com>
2025-09-07 17:04:10 +0400

    Mirationx fix

--- DAL/Migrations/202504051300_AddIndexes.cs
+++ DAL/Migrations/202504051300_AddIndexes.cs
@@ -10,29 +10,29 @@ namespace DAL.Migrations
             public override void Up()
             {
                 Create.Index("idx_employees_company_id")
-                    .OnTable("employees")
-                    .OnColumn("company_id");
+                    .OnTable("Employees")
+                    .OnColumn("CompanyId");
 
                 Create.Index("idx_employees_department_id")
-                    .OnTable("employees")
-                    .OnColumn("department_id");
+                    .OnTable("Employees")
+                    .OnColumn("DepartmentId");
 
                 Create.Index("idx_passports_employee_id")
-                    .OnTable("passports")
-                    .OnColumn("employee_id")
+                    .OnTable("Passports")
+                    .OnColumn("EmployeeId")
                     .Unique();
 
                 Create.Index("idx_companies_name")
-                    .OnTable("companies")
-                    .OnColumn("name");
+                    .OnTable("Companies")
+                    .OnColumn("Name");
             }
 
             public override void Down()
             {
-                Delete.Index("idx_companies_name").OnTable("companies");
-                Delete.Index("idx_passports_employee_id").OnTable("passports");
-                Delete.Index("idx_employees_department_id").OnTable("employees");
-                Delete.Index("idx_employees_company_id").OnTable("employees");
+                Delete.Index("idx_companies_name").OnTable("Companies");
+                Delete.Index("idx_passports_employee_id").OnTable("Passports");
+                Delete.Index("idx_employees_department_id").OnTable("Employees");
+                Delete.Index("idx_employees_company_id").OnTable("Employees");
             }
         }
     }
