75d9fda792ea05ada5a5e847087157193de0f593
unknown <sasha.lartsev@gmail.com>
2025-09-07 13:01:08 +0400

    InIt

---
+++ QueryParams/EmployeesQueryParams.cs
@@ -0,0 +1,10 @@
+﻿namespace QueryParams
+{
+    public class EmployeesQueryParams
+    {
+        public int? CompanyId { get; set; }
+        public int? DepartmentId { get; set; }
+        public string? CompanyName { get; set; }
+        public string? DepartmentName { get; set; }
+    }
+}
