e41a7dc402f9226136eb312afdda495b5be276f0
unknown <sasha.lartsev@gmail.com>
2025-09-07 14:59:37 +0400

    Migrations'

---
+++ DAL/Migrations/202504051200_CreateInitialTables.cs
@@ -0,0 +1,63 @@
+﻿using FluentMigrator;
+
+namespace DAL.Migrations
+{
+    
+
+    namespace MyApp.Migrations
+    {
+        [Migration(202504051200)]
+        public class CreateInitialTables : Migration
+        {
+            public override void Up()
+            {
+                Create.Table("companies")
+                    .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                    .WithColumn("name").AsString(255).NotNullable();
+
+                Create.Table("departments")
+                    .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                    .WithColumn("name").AsString(255).NotNullable()
+                    .WithColumn("phone").AsString(50).Nullable();
+
+                Create.Table("employees")
+                    .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                    .WithColumn("company_id").AsInt32().NotNullable()
+                    .WithColumn("department_id").AsInt32().Nullable()
+                    .WithColumn("name").AsString(100).NotNullable()
+                    .WithColumn("surname").AsString(100).NotNullable()
+                    .WithColumn("phone").AsString(50).Nullable();
+
+                Create.Table("passports")
+                    .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                    .WithColumn("employee_id").AsInt32().Unique().NotNullable()
+                    .WithColumn("type").AsString(50).NotNullable()
+                    .WithColumn("number").AsString(50).NotNullable();
+
+                Create.ForeignKey("fk_employees_company")
+                    .FromTable("employees").ForeignColumn("company_id")
+                    .ToTable("companies").PrimaryColumn("id");
+
+                Create.ForeignKey("fk_employees_department")
+                    .FromTable("employees").ForeignColumn("department_id")
+                    .ToTable("departments").PrimaryColumn("id");
+
+                Create.ForeignKey("fk_passports_employee")
+                    .FromTable("passports").ForeignColumn("employee_id")
+                    .ToTable("employees").PrimaryColumn("id");
+            }
+
+            public override void Down()
+            {
+                Delete.ForeignKey("fk_passports_employee").OnTable("passports");
+                Delete.ForeignKey("fk_employees_department").OnTable("employees");
+                Delete.ForeignKey("fk_employees_company").OnTable("employees");
+
+                Delete.Table("passports");
+                Delete.Table("employees");
+                Delete.Table("departments");
+                Delete.Table("companies");
+            }
+        }
+    }
+}
---
+++ DAL/Migrations/202504051300_AddIndexes.cs
@@ -0,0 +1,39 @@
+﻿using FluentMigrator;
+
+namespace DAL.Migrations
+{
+    namespace MyApp.Migrations
+    {
+        [Migration(202504051300)]
+        public class AddIndexes : Migration
+        {
+            public override void Up()
+            {
+                Create.Index("idx_employees_company_id")
+                    .OnTable("employees")
+                    .OnColumn("company_id");
+
+                Create.Index("idx_employees_department_id")
+                    .OnTable("employees")
+                    .OnColumn("department_id");
+
+                Create.Index("idx_passports_employee_id")
+                    .OnTable("passports")
+                    .OnColumn("employee_id")
+                    .Unique();
+
+                Create.Index("idx_companies_name")
+                    .OnTable("companies")
+                    .OnColumn("name");
+            }
+
+            public override void Down()
+            {
+                Delete.Index("idx_companies_name").OnTable("companies");
+                Delete.Index("idx_passports_employee_id").OnTable("passports");
+                Delete.Index("idx_employees_department_id").OnTable("employees");
+                Delete.Index("idx_employees_company_id").OnTable("employees");
+            }
+        }
+    }
+}
